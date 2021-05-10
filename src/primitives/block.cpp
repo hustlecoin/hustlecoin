6d84873a9928302b2b7344104990baa404d0b997
barrystyle <barrystyle@westnet.com.au>
2021-05-11 07:11:00 +0800

    crypto: Add scrypt N=1024 PoW
    Add scrypt pow hashing algorithm as a separate proof of work function, whilst
    retaining sha256d for standard blockhash/index functions only.
    
    Rebased from litecoin-project/litecoin@02961f0 without reintroducing old macros
    that were removed from strencodings.h in edb5bb3.

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -7,17 +7,26 @@
 
 #include <hash.h>
 #include <tinyformat.h>
+#include <crypto/scrypt.h>
 
 uint256 CBlockHeader::GetHash() const
 {
     return SerializeHash(*this);
 }
 
+uint256 CBlockHeader::GetPoWHash() const
+{
+    uint256 thash;
+    scrypt_1024_1_1_256((char*)&nVersion, (char*)&thash);
+    return thash;
+}
+
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
+    s << strprintf("CBlock(blkhash=%s, powhash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
         GetHash().ToString(),
+        GetPoWHash().ToString(),
         nVersion,
         hashPrevBlock.ToString(),
         hashMerkleRoot.ToString(),
